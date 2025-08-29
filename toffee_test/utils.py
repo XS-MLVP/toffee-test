f2d6ae1cb5efe0246714d67ed7732d946aebcaff
FrankOu2001 <wangjunyue2001@gmail.com>
2025-08-29 14:23:06 +0800

    Fix bug of merging coverage

--- toffee_test/utils.py
+++ toffee_test/utils.py
@@ -4,6 +4,9 @@ import subprocess
 import copy
 import json
 import base64
+import itertools
+import tempfile
+from typing import List, Iterable
 
 
 def exe_cmd(cmd):
@@ -39,7 +42,7 @@ def convert_line_coverage(line_coverage_list, output_dir):
             fign = [fign]
         assert isinstance(fign, list), f"Invalid data file: '{fign}'"
         assert os.path.exists(fdat), f"Invalid data file: '{fdat}'"
-        su, so, se = exe_cmd(["verilator_coverage  -write-info", fdat+".info", fdat])
+        su, so, se = exe_cmd(["verilator_coverage  -write-info", fdat + ".info", fdat])
         assert su, f"Failed to convert line coverage({fdat}): {se}"
         ignore_text = []
         # find all .ignore files
@@ -62,10 +65,10 @@ def convert_line_coverage(line_coverage_list, output_dir):
                     assert c not in ln, f"Invalid line number({i}): '{ln}'"
                 ignore_text.append(f"\'{ln}\'")
         su, so, se = exe_cmd(
-                    ["lcov", "--remove", fdat+".info", " ".join(ignore_text), "--output-file", fdat+".info"]
-                )
+            ["lcov", "--remove", fdat + ".info", " ".join(ignore_text), "--output-file", fdat + ".info"]
+        )
         assert su, f"Failed to remove line with file: '{ignore_file_list}', exception: {se}"
-        coverage_info_list.append(fdat+".info")
+        coverage_info_list.append(fdat + ".info")
         final_ignore_info.append([fdat, copy.deepcopy(ignore_file_list)])
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -76,45 +79,53 @@ def convert_line_coverage(line_coverage_list, output_dir):
     return parse_lines(so), final_ignore_info
 
 
-def lcov_merge_and_del(list_to_merge, out_file, max_kbytes = 10, deepth = 0):
-    import time
-    stamp = "/tmp/%s_MINFO_%s_" % (deepth, time.time()) + "%s.tmpinfo"
-    batch_input, merged_input = [], []
-    total_size = 0
-    batch_index = 0
-    for f in list_to_merge:
-        assert os.path.exists(f), f"Invalid file: '{f}'"
-        if total_size + len(f) > max_kbytes * 1024 and len(batch_input) > 1:
-            _lcov_del_merge(batch_input, stamp % batch_index)
-            total_size = 0
-            for f_del in batch_input:
-                os.remove(f_del)
-            batch_input = []
-            batch_index += 1
-            merged_input.append(stamp % batch_index)
-            continue
-        batch_input.append(f)
-        total_size += len(f)
-    if len(batch_input) > 0:
-        if len(merged_input) < 1:
-            _lcov_del_merge(batch_input, out_file)
-            return
-        else:
-            _lcov_del_merge(batch_input, stamp % batch_index)
-            merged_input.append(stamp % batch_index)
-    return lcov_merge_and_del(merged_input, out_file, max_kbytes, deepth + 1)
+def lcov_merge_and_del(list_to_merge: List[str], out_file: str, max_kbytes=10) -> None:
+    def create_temp_file() -> str:
+        _, path = tempfile.mkstemp(prefix="merge_", suffix=".info")
+        return path
 
-
-def _lcov_del_merge(list_to_merge, out_file):
-    if len(list_to_merge) == 1:
-        os.rename(list_to_merge[0], out_file)
-        return
-    if len(list_to_merge) == 0:
-        return
-    su, so, se = exe_cmd(["lcov", *["-a '%s'" % f.replace('[', '\\[').replace(']', '\\]') for f in list_to_merge], "-o", out_file])
-    assert su, f"Failed to merge line coverage: {se}"
+    max_bytes = max_kbytes * 1024
+    cmd_list = ["lcov", "-o", out_file, "-a", out_file]
+    cmd_len = len(" ".join(cmd_list)) + 1
+    temp_total_size = cmd_len
+    temp_files = []
+    total_size = cmd_len
+    raw = 0
+    cooking = 0
+    for cooking in range(len(list_to_merge)):
+        f = list_to_merge[cooking]
+        f_len = 3 + len(f)  # The length of '-a ' is 3
+        if f_len + total_size > max_bytes:
+            temp = create_temp_file()  # Create temp file
+            temp_files.append(temp)
+            temp_total_size += len(temp) + 3
+            _lcov_merge(itertools.islice(list_to_merge, raw, cooking), temp)  # Merge into the temp
+            total_size = cmd_len
+            raw = cooking
+        total_size += f_len
+    # There are still unmerged files here
+    temp = create_temp_file()
+    temp_files.append(temp)
+    _lcov_merge(itertools.islice(list_to_merge, raw, cooking + 1), temp)
+    # Delete .dat.info
     for f in list_to_merge:
         os.remove(f)
+    # If the number of temp files is also huge
+    if temp_total_size > max_bytes:
+        lcov_merge_and_del(temp_files, out_file)
+    else:
+        _lcov_merge(temp_files, out_file)
+    # Clean the temps
+    for f in temp_files:
+        os.remove(f)
+
+
+def _lcov_merge(list_to_merge: Iterable[str], out_file: str) -> None:
+    cmd = ["lcov", "-o", out_file]
+    for f in list_to_merge:
+        cmd.append("-a")
+        cmd.append(f)
+    subprocess.run(cmd, stdout=subprocess.PIPE, shell=False, check=True)
 
 
 def base64_encode(data):
